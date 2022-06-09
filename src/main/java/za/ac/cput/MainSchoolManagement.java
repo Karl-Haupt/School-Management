540ef7b9df7afdc093acf50651d50d286eeae4bf
Karl-Haupt <karlhaupt1@gmail.com>
2022-06-09 13:37:18 +0200

    Create Project Structure

---
+++ src/main/java/za/ac/cput/MainSchoolManagement.java
@@ -0,0 +1,11 @@
+package za.ac.cput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSchoolManagement {
+    public static void main( String[] args ) {
+        SpringApplication.run(MainSchoolManagement.class, args);
+    }
+}
