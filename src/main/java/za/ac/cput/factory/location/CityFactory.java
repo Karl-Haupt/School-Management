2ffe86671e01f6dea02a95b5d3f8acaf1c65cfb0
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2022-06-10 16:19:15 +0200

    Commiting Factory Class

--- src/main/java/za/ac/cput/factory/location/CityFactory.java
+++ src/main/java/za/ac/cput/factory/location/CityFactory.java
@@ -1,6 +1,11 @@
 package za.ac.cput.factory.location;
-
+/*
+ This is the city factory
+ Name & Surname: Charles Lemmert
+ Student No: 220498385
+*/
 import za.ac.cput.domain.location.City;
+import za.ac.cput.domain.location.Country;
 import za.ac.cput.helper.Validator;
 
 public class CityFactory {
