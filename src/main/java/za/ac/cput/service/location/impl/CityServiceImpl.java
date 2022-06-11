d6b395e4c692be818af1b998f6fccf40976a713a
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2022-06-11 21:54:00 +0200

    Completed Services code

--- src/main/java/za/ac/cput/service/location/impl/CityServiceImpl.java
+++ src/main/java/za/ac/cput/service/location/impl/CityServiceImpl.java
@@ -1,8 +1,8 @@
 package za.ac.cput.service.location.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.location.City;
-import za.ac.cput.repository.location.CityRepository;
 import za.ac.cput.repository.location.ICityRepository;
 import za.ac.cput.service.location.ICityService;
 
@@ -12,26 +12,26 @@ import java.util.Optional;
 public class CityServiceImpl implements ICityService {
     private ICityRepository cityRepository;
 
+    @Autowired
     private CityServiceImpl(ICityRepository cityRepository) {
         this.cityRepository = cityRepository;
     }
     @Override
     public City save(City city) {
-        return null;
+        return this.cityRepository.save(city);
     }
 
     @Override
-    public Optional<City> read(String s) {
-        return Optional.empty();
+    public Optional<City> read(String id) {
+        return this.cityRepository.findById(id);
     }
 
     @Override
     public void delete(City city) {
-
+        this.cityRepository.delete(city);
     }
-
     @Override
     public List<City> findAllCities() {
-        return null;
+        return this.cityRepository.findAll();
     }
 }
