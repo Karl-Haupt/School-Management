ce64465b17e2989494debe088695cbb37dc89819
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2022-06-11 19:43:10 +0200

    Committing Class created

---
+++ src/main/java/za/ac/cput/controller/location/CityController.java
@@ -0,0 +1,9 @@
+package za.ac.cput.controller.location;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/school-management/city/")
+public class CityController {
+}
---
+++ src/main/java/za/ac/cput/repository/location/CityRepository.java
@@ -0,0 +1,53 @@
+package za.ac.cput.repository.location;
+
+import za.ac.cput.domain.location.City;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/*
+ This is the city Repository
+ Name & Surname: Charles Lemmert
+ Student No: 220498385
+*/
+public class CityRepository {
+    private final List<City> cityList;
+    private static CityRepository cityRepo;
+
+    private CityRepository(){
+        this.cityList = new ArrayList<>();
+    }
+    public static CityRepository cityRepository(){
+        if(cityRepo == null) cityRepo = new CityRepository();
+        return cityRepo;
+    }
+    public City saveCity(City city){
+        //creates new city
+        Optional<City> readingCity = readCity(city.getId());
+        if(readingCity.isPresent() || readingCity.isEmpty()){
+            deleteCity(readingCity.get());
+        }
+        this.cityList.add(city);
+        return city;
+    }
+    public Optional<City> readCity(String id){
+        //reads existing city
+       return this.cityList.stream()
+               .filter(city -> city.getId().equalsIgnoreCase(id))
+               .findFirst();
+
+    }
+    public void deleteCity(City city){
+        //deletes a city by id
+        this.cityList.remove(city);
+
+
+    }
+
+    public List<City> returnAllCities(){
+        //returns all cities in the db
+        return this.cityList;
+    }
+
+}
